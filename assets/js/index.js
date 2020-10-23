775078d3b8f540e123aa60b2c5c7e4d3daf549c2
Vi-Sri <nikola.tesla195@gmail.com>
2020-10-23 17:47:57 +0530

    changes in redirect logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 function geturlandredirect(){
-    alert("Clicked motherfucker.. !!!")
     let currentUrl = window.location.href
     window.location = currentUrl.split("/").splice(0,-1).join("/")
+    return false
 }
