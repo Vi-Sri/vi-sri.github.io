9108e2ff9773e3c5c5cbebb435265ef2b3c2026a
Vi-Sri <nikola.tesla195@gmail.com>
2020-10-23 17:34:45 +0530

    changing page title

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 function geturlandredirect(){
+    alert("Clicked motherfucker.. !!!")
     let currentUrl = window.location.href
     window.location.href = currentUrl.split("/").splice(0,-1).join("/")
 }
